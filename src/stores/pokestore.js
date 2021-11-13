06d3bc70c09fa0a848e3ae2fb371ebb6d64cc08e
Mike Peiman <mikepeiman@gmail.com>
2021-11-13 18:04:18 -0500

    added pokestore.js, following along with James D Quick https://www.youtube.com/watch?v=UU7MgYIbtAk

---
+++ src/stores/pokestore.js
@@ -0,0 +1,12 @@
+const fetchPokemon = async () => {
+    const url = 'https://pokeapi.co/api/v2/pokemon?limit=150/'
+    const res = await fetch(url)
+    const data = await res.json()
+    const loadedPokemon = data.results.map((data, index) => {
+        return {
+            name: data.name,
+            id: index + 1,
+            image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
+        }
+    })
+}
