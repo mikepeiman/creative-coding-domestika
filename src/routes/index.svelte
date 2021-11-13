7fc19706e00455e7c48438231c9110d0cef6338c
Mike Peiman <mikepeiman@gmail.com>
2021-11-13 12:55:48 -0500

    First commit - slight customization, Sveltekit skeleton project

---
+++ src/routes/index.svelte
@@ -0,0 +1,28 @@
+<script>
+
+</script>
+<div class="main">
+    
+    <h1>Welcome to MetaBrain</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap');
+    .main {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        font-family: 'Montserrat', sans-serif;
+    }
+
+    h1 {
+        font-size: 3rem;
+        font-weight: 900;
+        background: -webkit-linear-gradient(0deg, rgba(72,0,36,1) 0%,rgba(9,121,121,1) 25%, rgba(121,121,155,1) 50%, rgba(121,9,121,1) 75%, rgba(2,0,76,1) 100%);
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+    }
+
+</style>
