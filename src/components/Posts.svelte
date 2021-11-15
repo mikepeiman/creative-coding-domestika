3cb8ebd578243fc5901319d3ef30b764cce7742c
Mike Peiman <mikepeiman@gmail.com>
2021-11-14 19:35:46 -0500

    confirmed script module works from within component

--- src/components/Posts.svelte
+++ src/components/Posts.svelte
@@ -1,8 +1,17 @@
-<script>
-    export let posts
-    console.log(posts)
+<script context="module">
+	export const load = async ({ fetch }) => {
+		const res = await fetch('/posts.json');
+		if (res.ok) {
+			const { posts } = await res.json();
+			return { props: { posts } };
+		}
+	};
 </script>
 
+
+<script>
+    export let posts
+</script>
 
 {#each posts as post}
     <h1>{post.title}</h1>
