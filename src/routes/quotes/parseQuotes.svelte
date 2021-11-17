3a5b08853592bce5999d3a517e8c127735f7ff6a
Mike Peiman <mikepeiman@gmail.com>
2021-11-16 19:18:50 -0500

    Finally fixed my regex parsing problem! It's because they were escaped newline characters, in other words, the slash was in the text and I had to escape the slash in the regex

--- src/routes/quotes/parseQuotes.svelte
+++ src/routes/quotes/parseQuotes.svelte
@@ -37,20 +37,76 @@
 		const parser = new DOMParser();
 		const htmlDoc = parser.parseFromString(doc, 'text/html');
 		let divs = htmlDoc.getElementsByTagName('div');
-        for(let i = 0; i < divs.length; i++) {
-            discardBreaks(divs[i])
-        }
-		for (let i = 0; i < items.length; i++) {
-			parseQuote(items[i]);
+		for (let i = 0; i < 30; i++) {
+			if (discardBreaks(divs[i])) {
+				console.log(`That was ******** NOT ********* a break stub`);
+				let item = discardBreaks(divs[i]);
+				items = [...items, item];
+			} else {
+				console.log(`That was a break stub`);
+			}
 		}
+		for (let i = 0; i < 20; i++) {
+			parseQuote(items[i]);
+			// console.log(`typeof item ${typeof items[i]}`)
+			// removeLinebreaks(items[i])
+		}
+
+		let testString = 'abc\r\ndefg';
+		console.log(`🚀 ~ file: parseQuotes.svelte ~ line 56 ~ parseFile ~ testString`, testString);
+		let parsedSrting = testString.replaceAll(/(\r\n|\n|\r)/gm, '');
+		console.log(`🚀 ~ file: parseQuotes.svelte ~ line 57 ~ parseFile ~ parsedSrting`, parsedSrting);
 	}
 
-    function discardBreaks(item) {
-        console.log(`🚀 ~ file: parseQuotes.svelte ~ line 57 ~ discardBreaks ~ item`, item)
-        if(item.innerHTML.length > 5){
-            items = [...items, item]
-        }
-    }
+	function discardBreaks(item) {
+		if (item.textContent.length > 5) {
+            // let div = document.createElement('div')
+            // div.textContent = item.textContent
+            // let cleanedDiv = div.textContent.replace(/(\r\n|\n|\r)/gm, '@');
+            // console.log(`🚀 ~ file: parseQuotes.svelte ~ line 66 ~ discardBreaks ~ cleanedDiv`, cleanedDiv)
+			let stringifiedItem = item.textContent.toString();
+			// console.log(
+			// 	`🚀 ~ file: parseQuotes.svelte ~ line 64 ~ discardBreaks ~ stringifiedItem`,
+			// 	typeof stringifiedItem,
+			// 	stringifiedItem
+			// );
+			// for (let i = 0; i < stringifiedItem.length; i++) {
+			// 	console.log(
+			// 		`🚀 ~ file: parseQuotes.svelte ~ line 66 ~ discardBreaks ~ stringifiedItem[${i}]: `,
+			// 		stringifiedItem[i]
+			// 	);
+			// }
+			let parsedStringifiedItem = stringifiedItem.replaceAll(/(\r\n|\n|\r)/gm, '');
+			console.log(
+				`🚀 ~ file: parseQuotes.svelte ~ line 66 ~ discardBreaks ~ parsedStringifiedItem`,
+				parsedStringifiedItem
+			);
+
+			let resN = `${'\\n'.charCodeAt()}`;
+			console.log(`🚀 ~ file: parseQuotes.svelte ~ line 59 ~ discardBreaks ~ resN`, resN);
+			let resR = `${'\\r'.charCodeAt()}`;
+			console.log(`🚀 ~ file: parseQuotes.svelte ~ line 60 ~ discardBreaks ~ resR`, resR);
+			let toN = `${String.fromCharCode(10)}`;
+			console.log(`🚀 ~ file: parseQuotes.svelte ~ line 63 ~ discardBreaks ~ toN`, toN);
+			let toR = String.fromCharCode(92);
+			console.log(`🚀 ~ file: parseQuotes.svelte ~ line 65 ~ discardBreaks ~ toR`, toR);
+
+			// console.log(`charCode: '\n'.${charCodeAt()}`)
+			return item.textContent.replaceAll(/(\\r\\n|\\n|\\r)/gm, '');
+		}
+		return 0;
+	}
+
+	function removeLinebreaks(item) {
+		let str = item.textContent;
+		console.log(
+			`🚀 ~ file: parseQuotes.svelte ~ line 66 ~ removeLinebreaks ~ typeof str`,
+			typeof str
+		);
+		let replaced = str.replace(/(\r\n|\n|\r)/gm, '');
+		console.log(`🚀 ~ file: parseQuotes.svelte ~ line 68 ~ removeLinebreaks ~ replaced`, replaced);
+		return replaced;
+	}
 
 	// Quote structure from text:
 	// quotes are in `" "` quotation marks
@@ -65,24 +121,8 @@
 	// I will need also a flag or rating to determine which quotes are authenticated, or the degree of confidence, plus sources for this
 
 	function parseQuote(item) {
-        console.log(`🚀 ~ file: parseQuotes.svelte ~ line 63 ~ parseQuote ~ item`, item)
-        // let hasBreak = item.getElementsByTagName('br');
-		// if (!hasBreak.length) {
-        //     console.log(`🚀 ~ file: parseQuotes.svelte ~ line 66 ~ parseQuote ~ hasBreak`, hasBreak.length);
-		// }
-        // let innerHTML = item.innerHTML
-        //     if(innerHTML.length > 5){
-        //         console.log(`🚀 ~ file: parseQuotes.svelte ~ line 61 ~ parseQuote ~ item`, item)
+		console.log(`🚀 ~ file: parseQuotes.svelte ~ line 63 ~ parseQuote ~ item`, item);
 
-        //     }
-
-        // console.log(`🚀 ~ file: parseQuotes.svelte ~ line 67 ~ parseQuote ~ innerHTML`, innerHTML)
-        // console.log(`🚀 ~ file: parseQuotes.svelte ~ line 67 ~ parseQuote ~ innerHTML.length `, innerHTML.length)
-        // console.log(`🚀 ~ file: parseQuotes.svelte ~ line 67 ~ parseQuote ~ item.length`, item.length)
-
-        // replacementStr = replacementStr.replace(/\\n/g, '\n')
-
-		// discardEmptyDiv()
 		// parseQuoteText()
 		// parseAuthorName()
 		// parseAuthorCredential()
@@ -104,8 +144,7 @@
 
 {#if items.length}
 	{#each items as item}
-		<div class="card p-3 m-12 shadow-md">{@html item.innerHTML}</div>
-
+		<div class="card p-3 m-12 shadow-md">{item}</div>
 	{/each}
 {/if}
 
