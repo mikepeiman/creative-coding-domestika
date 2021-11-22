ac854aa08b9f6ee373dc6e89c16bc0e4e365ccb9
Mike Peiman <mikepeiman@gmail.com>
2021-11-22 11:54:26 -0500

    dgraph mutation currently working to add multiple quotes from data object in uploads/index.json.js; next need to get data from component (and clean code)

--- src/routes/upload/index.json.js
+++ src/routes/upload/index.json.js
@@ -24,7 +24,7 @@ mutation addQuotes($data: [AddQuoteInput!]!) {
     }
   }`)
 
-  const getQuotes = operationStore(`
+const getQuotes = operationStore(`
   query {
     quotes {
       quoteBody
@@ -34,8 +34,6 @@ mutation addQuotes($data: [AddQuoteInput!]!) {
     }
   }`)
 
-  let res = query(getQuotes)
-  console.log(`🚀 ~ file: index.json.js ~ line 38 ~ res`, res)
 
 // const addQuotes = mutation({
 //   query: query
@@ -55,50 +53,66 @@ function runAddQuotes() {
 
 
 const data = {
-  "data": [
-    {
-      "quoteBody": "It is not for nothing that our age cries out for the redeemer personality; for the one who can emancipate himself from the grip of the collective psychosis and save at least his own soul, who lights a beacon of hope for others, proclaiming that here is at least one man who has succeeded in extricating himself from the fatal identity within the group psyche.",
-      "author": {
-        "name": "Carl Jung",
-        "titleLong": "PhD"
+  data: [
+      {
+        "quoteBody": "It is not for nothing that our age cries out for the redeemer personality; for the one who can emancipate himself from the grip of the collective psychosis and save at least his own soul, who lights a beacon of hope for others, proclaiming that here is at least one man who has succeeded in extricating himself from the fatal identity within the group psyche.",
+        "author": {
+          "name": "Carl Jung",
+          "titleLong": "PhD"
+        },
+        "context": "individuality and mature selfhood",
+        "tags": {
+          "tag": "redemption, individuality, collective, mass psychosis"
+        }
       },
-      "context": "individuality and mature selfhood",
-      "tags": {
-        "tag": "redemption, individuality, collective, mass psychosis"
-      }
-    },
-    {
-      "quoteBody": "You don’t grow up until someone is more important than you",
-      "author": {
-        "name": "Jordan Peterson",
-        "titleLong": "Professor, University of Toronto"
+      {
+        "quoteBody": "To learn who rules over you, simply find out who you are not allowed to criticize",
+        "author": {
+          "name": "Voltaire",
+          "titleLong": ""
+        },
+        "context": "politics",
+        "tags": {
+          "tag": "politics, power"
+        }
       },
-      "context": "thinking about Melissa and parenting",
-      "tags": {
-        "tag": "maturity, parenting, growing up"
+      {
+        "quoteBody": "Test 444",
+        "author": {
+          "name": "Voltaire",
+          "titleLong": ""
+        },
+        "context": "politics",
+        "tags": {
+          "tag": "politics, power"
+        }
       }
-    }
-  ]
+    ]
 }
-export const get = async () => {
-  // return {
-  //   status: 200,
-  //   body: { data }
-  // }
 
-  try {
-    const query = query
-    await client.request(query).then((res) => {
-      console.log(`🚀 ~ file: index.json.js ~ line 75 ~ awaitclient.request ~ res`, res)
-      quotes = data.queryQuote
-    })
-    return {
-      status: 200,
-      body: { quotes }
-    }
-  } catch (error) {
-    return {
-      body: { error: 'There was a server error' }
-    }
+const addQuotesMutation = mutation(mutateQuotes)
+let res = addQuotesMutation(data)
+console.log(`🚀 ~ file: index.json.js ~ line 38 ~ res`, res)
+
+export const post = async () => {
+  return {
+    status: 200,
+    body: { data }
   }
+
+  // try {
+  //   const query = query
+  //   await client.request(query).then((res) => {
+  //     console.log(`🚀 ~ file: index.json.js ~ line 75 ~ awaitclient.request ~ res`, res)
+  //     quotes = data.queryQuote
+  //   })
+  //   return {
+  //     status: 200,
+  //     body: { quotes }
+  //   }
+  // } catch (error) {
+  //   return {
+  //     body: { error: 'There was a server error' }
+  //   }
+  // }
 }
--- src/routes/upload/index.svelte
+++ src/routes/upload/index.svelte
@@ -19,15 +19,22 @@
 		url: VITE_DGRAPH_ENDPOINT
 	});
 	import { gql, request } from 'graphql-request';
-import About from '../about.svelte';
+	import About from '../about.svelte';
 	export let id;
-	let quotesBody = '';
-	let author = '';
-	let context = '';
-	let tag = '';
-	let source = '';
-
-	const mutateQuotes = operationStore(`
+	let quoteBody = '';
+	let quoteAuthor = '';
+	let quoteContext = '';
+	let quoteTags = '';
+	let quoteSource = '';
+	let quote = {
+		body: quoteBody,
+		author: quoteAuthor,
+		context: quoteContext,
+		source: quoteSource,
+		tags: quoteTags
+	};
+	const mutateQuotes = mutation({
+		query: `
 		mutation addQuotes($data: [AddQuoteInput!]!) {
 			addQuote(input: $data) {
 			numUids
@@ -36,34 +43,81 @@ import About from '../about.svelte';
 				quoteBody
 			}
 			}
-		}`);
+		}`
+	});
+
+	const addNewQuote = mutation({
+		query: `
+		mutation addNewQuote {
+			addQuote(input: {quoteBody: "", author: {name: ""}}) {
+				numUids
+			}
+			}`
+	});
 
 	const getQuotes = operationStore(`query getQuotes {
-  queryQuote {
-    quoteBody
-    author {
-      name
-    }
-  }
-}
-`);
+		queryQuote {
+			quoteBody
+			author {
+			name
+			}
+		}
+		}
+		`);
+
+	const deleteTestQuotes = mutation({
+		query: `
+		mutation deleteTestQuotes {
+			deleteQuote(filter: {quoteBody: {anyoftext: "test"}})
+			}
+		`
+	});
 
 	let res = query(getQuotes);
+	// let res = query(deleteTestQuotes)
+
 	console.log(`🚀 ~ file: index.json.js ~ line 38 ~ res`, res);
 
-	export let data;
-	console.log(`🚀 ~ file: index.svelte ~ line 16 ~ data`, data);
-	let quoteAuthor, quoteBody, quoteContext, quoteSource, quoteTags;
+	// export let data;
 
 	function submitQuote() {
-		let quote = {
-			body: quoteBody,
-			author: quoteAuthor,
-			context: quoteContext,
-			source: quoteSource,
-			tags: quoteTags
+		console.log(`🚀 ~ file: index.svelte ~ line 74 ~ submitQuote ~ quote`, quote);
+		try {
+			let data = [];
+			data = [...data, quote];
+			mutateQuotes({ data }).then((res) => {
+				console.log(res.data, res.error);
+			});
+		} catch (error) {
+			console.log(`🚀 ~ file: index.svelte ~ line 80 ~ submitQuote ~ error`, error);
+		}
+
+		addNewQuote({ quoteBody: quote.quoteBody, authorName: quote.quoteAuthor }).then((res) => {
+			console.log(`addNewQuote: `, res.data, res.error);
+		});
+		// const res1 = mutateQuotes({ quote });
+		// console.log(`🚀 ~ file: index.svelte ~ line 78 ~ submitQuote ~ res1`, res1)
+		// try {
+		// 	deleteTestQuotes({});
+		// } catch (error) {
+		// 	console.log(`🚀 ~ file: index.svelte ~ line 87 ~ submitQuote ~ error`, error);
+		// }
+		// const res2 = deleteTestQuotes({})
+		// console.log(`🚀 ~ file: index.svelte ~ line 80 ~ submitQuote ~ res2`, res2)
+	}
+
+	function endpoint() {
+		const fire = async () => {
+			const res = await fetch('/upload.json');
+			if (res.ok) {
+				const { data } = await res.json();
+                console.log(`🚀 ~ file: index.svelte ~ line 114 ~ fire ~ data`, data)
+				return { props: { data } };
+			} else {
+				return 'there was an error'
+			}
 		};
-		console.log(`🚀 ~ file: index.svelte ~ line 27 ~ submitQuote ~ quote`, quote);
+		fire()
 	}
 </script>
 
@@ -77,19 +131,16 @@ import About from '../about.svelte';
 </div> -->
 
 <div class="quotes">
-
 	{#if $getQuotes.fetching}
-	<p>Loading...</p>
+		<p>Loading...</p>
 	{:else if $getQuotes.error}
-	<p>Error: {$getQuotes.Error.message}</p>
+		<p>Error: {$getQuotes.Error.message}</p>
 	{:else}
-
-	{#each $getQuotes.data.queryQuote as item, i}
-		<ul>
-			<li><h1>{i+1}: {item.quoteBody} - {item.author.name}</h1>
-			</li>
-		</ul>
-	{/each}
+		{#each $getQuotes.data.queryQuote as item, i}
+			<ul>
+				<li><h1>{i + 1}: {item.quoteBody} - {item.author.name}</h1></li>
+			</ul>
+		{/each}
 	{/if}
 </div>
 
@@ -122,6 +173,9 @@ import About from '../about.svelte';
 		<input bind:value={quoteSource} type="text" placeholder="Source" class="input" />
 	</div>
 	<button type="submit" class="btn btn-outline" on:click={() => submitQuote()}>SUBMIT</button>
+	<button type="submit" class="btn btn-outline" on:click={() => endpoint()}
+		>Hit .json endpoint</button
+	>
 </div>
 
 <h1>{quoteBody} - {quoteAuthor}</h1>
