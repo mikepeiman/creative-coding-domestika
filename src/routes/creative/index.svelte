e8eddf4475aa5e76664d84c8e40ae9c166676fb7
Mike Peiman <mikepeiman@gmail.com>
2021-11-28 12:47:17 -0500

    concluding first lesson on canvas rect and arc

--- src/routes/creative/index.svelte
+++ src/routes/creative/index.svelte
@@ -9,18 +9,21 @@
 	let canvasInit = () => {
 		canvas = document.querySelector('canvas');
 		context = canvas.getContext('2d');
-        context.fillStyle = "#0099ff"; // I've discovered that fillStyle must precede .fillRect or it does not apply
+		context.fillStyle = '#0099ff'; // I've discovered that fillStyle must precede .fillRect or it does not apply
 		// context.fillRect(100, 100, 400, 400);
-        context.beginPath()
-        context.rect(100,100,400,400)
-        context.fill()
-        context.lineWidth = 4 // similarly, lineWidth must precede .stroke()
-        context.stroke()
+		context.beginPath();
+		context.rect(100, 100, 400, 400);
+		context.fill();
+		context.lineWidth = 4; // similarly, lineWidth must precede .stroke()
+		context.stroke();
+		drawArc(300, 300, 100, 0, Math.PI * 2);
 	};
 
-    let drawArc = () => {
-
-    }
+	let drawArc = (x, y, r, start, end) => {
+        context.beginPath();
+		context.arc(x, y, r, start, end);
+        context.stroke();
+	};
 </script>
 
 <div class="flex flex-col content-center items-center w-full">
