4acb85a1bc093c6870d1515c30532505d7b2878d
Mike Peiman <mikepeiman@gmail.com>
2021-11-28 13:17:01 -0500

    Implementing some dynamic drawing functions

--- src/routes/creative/index.svelte
+++ src/routes/creative/index.svelte
@@ -4,25 +4,31 @@
 	let canvas, context;
 	onMount(() => {
 		canvasInit();
+		drawOnCanvas();
 	});
 
 	let canvasInit = () => {
 		canvas = document.querySelector('canvas');
 		context = canvas.getContext('2d');
 		context.fillStyle = '#0099ff'; // I've discovered that fillStyle must precede .fillRect or it does not apply
-		// context.fillRect(100, 100, 400, 400);
-		context.beginPath();
-		context.rect(100, 100, 400, 400);
-		context.fill();
-		context.lineWidth = 4; // similarly, lineWidth must precede .stroke()
-		context.stroke();
-		drawArc(300, 300, 100, 0, Math.PI * 2);
 	};
 
 	let drawArc = (x, y, r, start, end) => {
-        context.beginPath();
+		context.beginPath();
 		context.arc(x, y, r, start, end);
-        context.stroke();
+		context.stroke();
+	};
+	let drawRect = (originX, originY, width, height, lineWidth) => {
+		context.beginPath();
+		context.rect(originX, originY, width, height);
+		context.fill();
+		context.lineWidth = lineWidth;
+		context.stroke();
+	};
+	let drawOnCanvas = () => {
+		for (let i = 0; i < 5; i++) {
+			drawRect(100 + 60 * i, 100, 40, 40, 5);
+		}
 	};
 </script>
 
