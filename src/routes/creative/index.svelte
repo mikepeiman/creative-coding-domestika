dc46d82b12e0210ba5f4bfdb5f3832b0b64b2559
Mike Peiman <mikepeiman@gmail.com>
2021-11-28 10:56:46 -0500

    exploring basic rect

--- src/routes/creative/index.svelte
+++ src/routes/creative/index.svelte
@@ -1,19 +1,31 @@
 <!-- https://www.domestika.org/en/courses/2729-creative-coding-making-visuals-with-javascript/units/9668-fundamentals -->
 <script>
-    import {onMount} from 'svelte'
-    let canvas, context
-    onMount(() => {
-        canvas = document.querySelector('canvas')
-        context = canvas.getContext('2d')
-        context.fillRect(100,100,400,400)
-    })
-    
-    
-    </script>
-    
-    <div class="flex flex-col content-center items-center w-full">
-        <h1 class="text-sky-900 w-full text-center text-3xl bold">Creative Coding</h1>
-        <div class="flex w-full items-center justify-center m-12">
-            <canvas class="border-2 border-yellow-600" width="600" height="600"></canvas>
-        </div>
-    </div>
+	import { onMount } from 'svelte';
+	let canvas, context;
+	onMount(() => {
+		canvasInit();
+	});
+
+	let canvasInit = () => {
+		canvas = document.querySelector('canvas');
+		context = canvas.getContext('2d');
+        context.fillStyle = "#0099ff"; // I've discovered that fillStyle must precede .fillRect or it does not apply
+		// context.fillRect(100, 100, 400, 400);
+        context.beginPath()
+        context.rect(100,100,400,400)
+        context.fill()
+        context.lineWidth = 4 // similarly, lineWidth must precede .stroke()
+        context.stroke()
+	};
+
+    let drawArc = () => {
+
+    }
+</script>
+
+<div class="flex flex-col content-center items-center w-full">
+	<h1 class="text-sky-300 w-full text-center text-3xl bold">Creative Coding</h1>
+	<div class="flex w-full items-center justify-center m-12">
+		<canvas class="border-2 border-yellow-600" width="600" height="600" />
+	</div>
+</div>
