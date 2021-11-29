ed14d51109146d5c360bab6e73812cb2d1b7c50f
Mike Peiman <mikepeiman@gmail.com>
2021-11-29 09:57:41 -0500

    first canvas-sketch working - custom colors not working yet

--- src/routes/creative/drawing.js
+++ src/routes/creative/drawing.js
@@ -15,60 +15,19 @@ let drawArc = (x, y, r, start, end) => {
 	context.arc(x, y, r, start, end);
 	context.stroke();
 };
-export let drawRect = (context, i, j, originX, originY, width, height, gap, lineWidth, fill) => {
+export const drawRect = (context, originX, originY, width, height, gap, lineWidth, fill) => {
 	context.strokeStyle = "#aabbff"
 	context.beginPath();
-	// originX = width + ((width + gap) * i)
-	// originY = height + ((height + gap) * j)
 	context.rect(originX, originY, width, height);
 	context.lineWidth = lineWidth;
 	context.stroke();
-	// context.strokeStyle = "#aabbff"
-	context.fillStyle = fill; //setItemColor(i, j, totalItems)
+	context.fillStyle = fill;
 	context.fill();
 };
 
-function setItemColor(i, j, totalItems) {
+export const setItemColor = (i, j, totalItems) => {
 	let hueOffset = 30
 	let hueInterval = 360 / (totalItems)
 	let currentFactor = i + j
 	return `hsla(${(currentFactor + (i*.75)) * hueInterval + hueOffset}, 45%, 45%, .4)`;
-}
-
-let canvas, context, innerWidth, innerHeight;
-let itemsPerLine,
-	itemsPerColumn,
-	widthPerItem,
-	heightPerItem,
-	gapPercentage,
-	widthPercentage,
-	heightPercentage,
-	gap,
-	width,
-	height,
-	originX,
-	originY,
-	pageMargin,
-	totalItems;
-gap = 10
-width = 50
-height = 50
-itemsPerColumn = 5
-itemsPerLine = 5
-totalItems = itemsPerLine * itemsPerColumn
-let drawRectData = (i, j, originX, originY, width, height, gap, lineWidth, fill) => {
-	originX = width + (width + gap) * i
-	originX
-	originY = height + (height + gap) * j
-	originY
-	let rect = [originX, originY, width, height]
-    console.log(`rect`, rect)
-
-};
-for (let j = 0; j < itemsPerColumn; j++) {
-	for (let i = 0; i < itemsPerLine; i++) {
-		let fill = `#${i}${i}${i}${j}${j}${j}`;
-		drawRectData( i, j, originX, originY, width, height, gap, 5, fill);
-		drawRectData( i, j, originX + 8, originY + 8, width - 16, height - 16, gap, 5, fill);
-	}
 }
--- src/routes/creative/sketch-01.js
+++ src/routes/creative/sketch-01.js
@@ -1,13 +1,36 @@
 const canvasSketch = require('canvas-sketch');
+import { drawRect, setItemColor } from './drawing'
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [2048, 2048]
 };
-
+let h, w, gap, fill, itemsPerColumn, itemsPerLine, originX, originY, totalItems
+h = w = gap = 50
+originX = originY = 100
+itemsPerColumn = itemsPerLine = 6
+totalItems - itemsPerLine * itemsPerColumn
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+    drawGrid()
+    function drawGrid() {
+      context.clearRect(0, 0, width, height);
+      for (let j = 0; j < itemsPerColumn; j++) {
+        for (let i = 0; i < itemsPerLine; i++) {
+          fill = `hsla(180, 50%, 50%, .2)`;
+          let x = originX + (w + gap) * i;
+          let y = originY + (h + gap) * j;
+
+          drawRect(context, x, y, w, h, gap, 5, fill);
+          if (Math.random() > 0.5) {
+            fill = setItemColor(i, j, totalItems);
+            drawRect(context, x + 8, y + 8, w - 16, h - 16, gap, 5, fill);
+          }
+          fill = `hsla(180, 50%, 50%, .2)`;
+        }
+      }
+    }
   };
 };
 
