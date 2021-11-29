815f974980231d4d6e8209ca75c7a25ffa255db5
Mike Peiman <mikepeiman@gmail.com>
2021-11-29 18:56:32 -0500

    Experimenting with variations

--- src/routes/creative/sketch-02.js
+++ src/routes/creative/sketch-02.js
@@ -1,6 +1,7 @@
 const canvasSketch = require('canvas-sketch');
 const math = require('canvas-sketch-util/math')
 const random = require('canvas-sketch-util/random')
+import { setItemColor} from './drawing.js'
 
 const settings = {
   dimensions: [1080, 1080]
@@ -18,9 +19,9 @@ const sketch = () => {
     const w = width * 0.01;
     const h = height * 0.1;
 
-    const num = 12
+    const num = 55
     const radius = width * 0.3
-
+    let color = `hsla(180, 50%, 50%, 1)`
     for (let i = 0; i < num; i++) {
       const slice = math.degToRad(360 / num)
       const angle = slice * i
@@ -36,12 +37,26 @@ const sketch = () => {
       context.save()
       context.translate(x, y);
       // context.translate(cx, cy);
-      context.rotate(-angle);
-      context.scale(random.range(.1,3), random.range(.2,4))
+      context.rotate(random.range(-angle + .3, -angle - .3));
+      context.scale(random.range(.3,3), random.range(.2,2))
 
       context.beginPath();
-      context.rect(-w * 0.5, -h * 0.5, w, h);
+      context.rect(random.range(-w * 0.5,-w * 1.5), random.range(0, -h * 0.5), w, h);
+      color = `hsla(${random.range(0, 360)}, ${random.range(15,85)}%, ${random.range(15,85)}%, ${random.range(0.05, 1)})`
+      context.fillStyle = color
       context.fill();
+      context.restore()
+
+      context.save()
+      context.translate(cx,cy)
+      context.rotate(-angle)
+      context.beginPath()
+      context.arc(0, 0, radius * random.range(0.75,1.25), slice * random.range(-15, 5), slice * random.range(11.5,0.5))
+      context.lineWidth = random.range(5, 20)
+      color = `hsla(${random.range(0, 360)}, ${random.range(15,85)}%, ${random.range(15,85)}%, ${random.range(0.05, 1)})`
+      console.log(`🚀 ~ file: sketch-02.js ~ line 55 ~ return ~ color`, color)
+      context.strokeStyle = color
+      context.stroke()
       context.restore()
     }
   };
