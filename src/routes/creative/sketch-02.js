8c565365c903d285f078048e0b1fd6027a89f41e
Mike Peiman <mikepeiman@gmail.com>
2021-11-29 14:17:01 -0500

    example of using context.save() and .restore() to separate operations

--- src/routes/creative/sketch-02.js
+++ src/routes/creative/sketch-02.js
@@ -7,17 +7,28 @@ const settings = {
 const sketch = () => {
   return ({ context, width, height }) => {
     // context.clearRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = '#333'; // fillStyle must come after fillRect or I won't see the new rect!!
     context.fillRect(0, 0, width, height);
-    context.fillStyle = '#0033aa'; // fillStyle must come after fillRect or I won't see the new rect!!
+    context.fillStyle = '#ff33aa'; // fillStyle must come after fillRect or I won't see the new rect!!
     const x = width * 0.5;
     const y = height * 0.5;
     const w = width * 0.3;
     const h = height * 0.3;
+
+    context.save()
+
     context.translate(x, y);
-    context.rotate(0.3);
+    context.rotate(1);
     context.beginPath();
-    context.rect(0, 0, w, h);
+    context.rect(-w * 0.5, -h * 0.5, w, h);
     context.fill();
+
+    context.restore()
+    context.translate(200, 200)
+    context.beginPath()
+    context.arc(0, 0, 50, 0, Math.PI * 2)
+    context.fillStyle = '#aa33ff'; // fillStyle must come after fillRect or I won't see the new rect!!
+    context.fill()
   };
 };
 
