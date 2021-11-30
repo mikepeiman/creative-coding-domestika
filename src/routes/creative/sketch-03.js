0ea2d9a141b5fb9ed7a780ff26c8359492bde9ac
Mike Peiman <mikepeiman@gmail.com>
2021-11-30 11:37:42 -0500

    refactored to classes, cleaner code as per tutorial

--- src/routes/creative/sketch-03.js
+++ src/routes/creative/sketch-03.js
@@ -9,17 +9,11 @@ const sketch = () => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    const pointA = new Point(800, 400, 10)
-    const pointB = new Point(300, 700 , 10)
-    context.beginPath()
-    context.arc(pointA.x, pointA.y, pointA.radius, 0, Math.PI * 2)
-    context.fillStyle = 'black'
-    context.fill()
-    context.beginPath()
-    context.arc(pointB.x, pointB.y, pointB.radius, 0, Math.PI * 2)
-    context.fillStyle = 'black'
-    context.fill()
+    const agentA = new Agent(800, 400)
+    const agentB = new Agent(300, 700)
 
+    agentA.draw(context)
+    agentB.draw(context)
   };
 };
 
@@ -30,7 +24,20 @@ class Point {
   constructor(x, y, radius) {
     this.x = x
     this.y = y
-    this.radius = radius
+    
+  }
+}
 
+class Agent {
+  constructor(x, y) {
+    this.pos = new Point(x, y)
+    this.radius = 10
+  }
+
+  draw(context) {
+    context.fillStyle = 'black'
+    context.beginPath()
+    context.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2)
+    context.fill()
   }
 }
