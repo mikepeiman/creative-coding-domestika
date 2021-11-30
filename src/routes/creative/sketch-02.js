118ecb03a5b55caef827a53ccaf1c94f5c5e633a
Mike Peiman <mikepeiman@gmail.com>
2021-11-29 19:25:45 -0500

    more exploration, it's fun. adding image next

--- src/routes/creative/sketch-02.js
+++ src/routes/creative/sketch-02.js
@@ -16,10 +16,10 @@ const sketch = () => {
     const cx = width * 0.5;
     const cy = height * 0.5;
     let x, y
-    const w = width * 0.01;
-    const h = height * 0.1;
+    let w = width * 1;
+    let h = height * .01;
 
-    const num = 55
+    const num = 300
     const radius = width * 0.3
     let color = `hsla(180, 50%, 50%, 1)`
     for (let i = 0; i < num; i++) {
@@ -28,36 +28,58 @@ const sketch = () => {
 
       x = cx + radius * Math.sin(angle)
       y = cy + radius * Math.cos(angle)
+      w = random.range(width * 0.1, width * 1.5)
+      h = random.range(height * 0.001, height * .05)
       // =======================================================================
       // Alternative method is to switch the x, y assignments to the below, and uncomment the additional transate (keep both active)
       // =======================================================================
       // x = radius * Math.sin(angle)
       // y = radius * Math.cos(angle)
+      // =======================================================================
 
       context.save()
-      context.translate(x, y);
+      // context.translate(x, y);
+      // =======================================================================
+      // make the lines originate from 0, 0 corner in flare, or around a circular arc
+      // =======================================================================
       // context.translate(cx, cy);
-      context.rotate(random.range(-angle + .3, -angle - .3));
+      // =======================================================================
+      context.rotate(random.range(-angle * 5.5, -angle * -.1));
       context.scale(random.range(.3,3), random.range(.2,2))
 
       context.beginPath();
       context.rect(random.range(-w * 0.5,-w * 1.5), random.range(0, -h * 0.5), w, h);
-      color = `hsla(${random.range(0, 360)}, ${random.range(15,85)}%, ${random.range(15,85)}%, ${random.range(0.05, 1)})`
+      color = `hsla(${random.range(280, 40)}, ${random.range(65,85)}%, ${random.range(15,85)}%, ${random.range(0.2, .4)})`
       context.fillStyle = color
       context.fill();
       context.restore()
 
       context.save()
-      context.translate(cx,cy)
+      // =======================================================================
+      // The following sets circle origin to center
+      // =======================================================================
+      // context.translate(cx,cy)
+      // =======================================================================
       context.rotate(-angle)
       context.beginPath()
-      context.arc(0, 0, radius * random.range(0.75,1.25), slice * random.range(-15, 5), slice * random.range(11.5,0.5))
-      context.lineWidth = random.range(5, 20)
-      color = `hsla(${random.range(0, 360)}, ${random.range(15,85)}%, ${random.range(15,85)}%, ${random.range(0.05, 1)})`
-      console.log(`🚀 ~ file: sketch-02.js ~ line 55 ~ return ~ color`, color)
-      context.strokeStyle = color
+      context.arc(0, 0, radius * random.range(.25,4.25), slice * random.range(-15, 5), slice * random.range(11.5,0.5))
+      context.lineWidth = random.range(15, 70)
+      color = `hsla(${random.range(180, 240)}, ${random.range(65,85)}%, ${random.range(15,85)}%, ${random.range(0.1, .5)})`
+      // =======================================================================
+      // try gradients
+      // =======================================================================
+      let color1 = `hsla(${random.range(180, 240)}, ${random.range(65,85)}%, ${random.range(15,85)}%, ${random.range(0.3, .8)})`
+      let color2 = `hsla(${random.range(240, 300)}, ${random.range(65,85)}%, ${random.range(15,85)}%, ${random.range(0.5, 1)})`
+      let grd = context.createLinearGradient(0, 0, cx, cy)
+      grd.addColorStop(0, color1)
+      grd.addColorStop(1, color2)
+
+      context.strokeStyle = grd
+      // context.strokeStyle = color
+      // =======================================================================
       context.stroke()
       context.restore()
+
     }
   };
 };
