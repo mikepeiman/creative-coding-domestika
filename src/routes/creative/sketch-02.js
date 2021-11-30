fa8f8df2164f8d1464903bfd291776345f8ef500
Mike Peiman <mikepeiman@gmail.com>
2021-11-30 15:07:21 -0500

    progress towards a reasonable animation; building an Arc class

--- src/routes/creative/sketch-02.js
+++ src/routes/creative/sketch-02.js
@@ -106,6 +106,30 @@ const sketch = ({ context, width, height }) => {
   // ===========================================================================
   color = `hsla(${random.range(0, 360)}, ${random.range(10, 60)}%, ${random.range(10, 60)}%, ${random.range(0.1, 0.9)})`
 
+  color1 = `hsla(${random.range(200, 140)}, ${random.range(25, 75)}%, ${random.range(5, 45)}%, ${random.range(0.25, 0.85)})`
+  color2 = `hsla(${random.range(40, 120)}, ${random.range(25, 75)}%, ${random.range(5, 45)}%, ${random.range(0.25, 0.85)})`
+  color3 = `hsla(${random.range(120, 180)}, ${random.range(25, 75)}%, ${random.range(5, 45)}%, ${random.range(0.25, 0.85)})`
+  color4 = `hsla(${random.range(240, 160)}, ${random.range(25, 75)}%, ${random.range(5, 45)}%, ${random.range(0.25, 0.85)})`
+  color5 = `hsla(${random.range(120, 240)}, ${random.range(25, 75)}%, ${random.range(5, 45)}%, ${random.range(0.25, 0.85)})`
+  color6 = `hsla(${random.range(120, 240)}, ${random.range(45, 75)}%, ${random.range(25, 45)}%, ${random.range(0.55, 0.85)})`
+
+  let gradientColorValues = [
+    {
+      stop: random.range(0, 0.2), color1
+    },
+    {
+      stop: random.range(0.2, 0.4), color2
+    },
+    {
+      stop: random.range(0.4, 0.6), color3
+    },
+    {
+      stop: random.range(0.6, 0.8), color4
+    },
+    {
+      stop: random.range(0.8, 1), color5
+    },
+  ]
   // ===========================================================================
   // Main loop - animates
   // ===========================================================================
@@ -121,12 +145,13 @@ const sketch = ({ context, width, height }) => {
     radius = width * .3
 
     // =========================================================================
-
     // =============================================================================
-
-    // context.fillStyle = '#000'; 
-    // context.fillRect(off3, off3, width - off3*2, height - off3*2);
-    const num = 17
+    // The main interior loop counter
+    // =============================================================================
+    const num = 15
+    // =========================================================================
+    // highlighting interior loop counter
+    // =========================================================================
     for (let i = 0; i < num; i++) {
       radius = width * random.range(0.1, .4)
       slice = math.degToRad(360 / num)
@@ -139,102 +164,35 @@ const sketch = ({ context, width, height }) => {
       w = random.range(width * .5, width * 1.5)
       h = random.range(height * .1, height * .5)
 
-      // =======================================================================
-      // Alternative method is to switch the x, y assignments to the below, and uncomment the additional transate (keep both active)
-      // =======================================================================
-      // x = radius * Math.sin(angle)
-      // y = radius * Math.cos(angle)
-      // =======================================================================
-
-      // context.translate(x, y);
-      // =======================================================================
-      // make the lines originate from 0, 0 corner in flare, or around a circular arc
-      // =======================================================================
-      // context.translate(cx, cy);
-      // =======================================================================
-
-      // context.save()
-      // context.rotate(random.range(-angle * 5.5, -angle * -.1));
-      // context.scale(random.range(.3,3), random.range(.2,2))
-      // context.beginPath();
-      // context.rect(random.range(-w * 0.5,-w * 1.5), random.range(0, -h * 0.5), w, h);
-      // color = `hsla(${random.range(280, 40)}, ${random.range(65,85)}%, ${random.range(15,85)}%, ${random.range(0.2, .4)})`
-      // context.fillStyle = color
-      // context.fill();
-      // context.restore()
-      // =============================================================================
-      // change rect to line
-      // =============================================================================
-      // for(let j = 0; j < 10; j++){
-
-      //   context.save()
-      //   context.rotate(random.range(-angle * 5.5, -angle * -.1));
-      //   context.scale(random.range(1,3), random.range(.2,2))
-      //   context.beginPath();
-      //   context.moveTo(x,y)
-      //   context.lineTo(cx, cy);
-      //   context.lineCap = 'round'
-      //   color = `hsla(${random.range(320, 80)}, ${random.range(65,85)}%, ${random.range(55,15)}%, ${random.range(0.05, .25)})`
-      //   context.strokeStyle = color
-      //   context.lineWidth = random.range(10, 50)
-      //   context.fill();
-      //   context.stroke()
-      //   context.closePath()
-      //   context.restore()
-      // }
-
-
       context.save()
       // =======================================================================
       // The following sets circle origin to center
-      // =======================================================================
       context.translate(cx, cy)
+      // =======================================================================
+      // =======================================================================
+      // set random arc origins
+      // =======================================================================
+      let r1 = random.range(-50, 50)
+      let r2 = random.range(-50, 50)
+      // context.translate(cx + i * r1, cy + i * r2)
       // =======================================================================
       context.rotate(-angle)
       context.beginPath()
       context.arc(0, 0, radius * random.range(1, .4), slice * random.range(.25, .21), slice * random.range(.05, .2))
+      context.shadowBlur = random.range(50, 200)
+      context.shadowColor = color6
       // context.lineCap = 'round'
       context.lineWidth = random.range(5, 250)
       color = `hsla(${random.range(0, 60)}, ${random.range(65, 85)}%, ${random.range(15, 85)}%, ${random.range(0.05, .95)})`
-      // =======================================================================
-      // try gradients
-      // =======================================================================
-      //  color1 = `hsla(${random.range(180, 240)}, ${random.range(65,85)}%, ${random.range(15,85)}%, ${random.range(0.1, .5)})`
-      //  color2 = `hsla(${random.range(240, 360)}, ${random.range(65,85)}%, ${random.range(15,85)}%, ${random.range(0.25, .4)})`
-      //  color3 = `hsla(${random.range(0, 90)}, ${random.range(65,85)}%, ${random.range(15,85)}%, ${random.range(0.1, .5)})`
-      //  color4 = `hsla(${random.range(90, 180)}, ${random.range(65,85)}%, ${random.range(15,85)}%, ${random.range(0.25, .4)})`
-      //  color5 = `hsla(${random.range(0, 360)}, ${random.range(25,85)}%, ${random.range(25,55)}%, ${random.range(0.5, .75)})`
-      // =======================================================================
-      //  color1 = `hsla(${random.range(180, 240)}, ${random.range(0,100)}%, ${random.range(15,85)}%, ${random.range(0.1, .5)})`
-      //  color2 = `hsla(${random.range(240, 360)}, ${random.range(0,100)}%, ${random.range(15,85)}%, ${random.range(0.1, .4)})`
-      //  color3 = `hsla(${random.range(0, 90)}, ${random.range(0,100)}%, ${random.range(15,85)}%, ${random.range(0.1, .5)})`
-      //  color4 = `hsla(${random.range(120, 240)}, ${random.range(40,100)}%, ${random.range(15,85)}%, ${random.range(0.1, .5)})`
-      //  color5 = `hsla(${random.range(0, 360)}, ${random.range(0,100)}%, ${random.range(15,85)}%, ${random.range(0.1, .75)})`
-      // =======================================================================
-      //  color1 = `hsla(${random.range(180, 240)}, ${random.range(25,75)}%, ${random.range(15,55)}%, ${random.range(0.25,0.85)})`
-      //  color2 = `hsla(${random.range(240, 360)}, ${random.range(25,75)}%, ${random.range(15,55)}%, ${random.range(0.25,0.85)})`
-      //  color3 = `hsla(${random.range(0, 120)}, ${random.range(25,75)}%, ${random.range(15,55)}%, ${random.range(0.25,0.85)})`
-      //  color4 = `hsla(${random.range(120, 240)}, ${random.range(25,75)}%, ${random.range(15,55)}%, ${random.range(0.25,0.85)})`
-      //  color5 = `hsla(${random.range(0, 360)}, ${random.range(25,75)}%, ${random.range(15,55)}%, ${random.range(0.25,0.85)})`
-      // =======================================================================
-      // these ones are awesome!
+
       color1 = `hsla(${random.range(0, 60)}, ${random.range(25, 75)}%, ${random.range(5, 45)}%, ${random.range(0.25, 0.85)})`
       color2 = `hsla(${random.range(240, 360)}, ${random.range(25, 75)}%, ${random.range(5, 45)}%, ${random.range(0.25, 0.85)})`
       color3 = `hsla(${random.range(0, 120)}, ${random.range(25, 75)}%, ${random.range(5, 45)}%, ${random.range(0.25, 0.85)})`
       color4 = `hsla(${random.range(120, 240)}, ${random.range(25, 75)}%, ${random.range(5, 45)}%, ${random.range(0.25, 0.85)})`
       color5 = `hsla(${random.range(0, 40)}, ${random.range(25, 75)}%, ${random.range(5, 45)}%, ${random.range(0.25, 0.85)})`
       color6 = `hsla(${random.range(120, 240)}, ${random.range(45, 75)}%, ${random.range(25, 45)}%, ${random.range(0.55, 0.85)})`
-      // =======================================================================
-      // =======================================================================
-      let grd = context.createLinearGradient(random.range(0, 100), random.range(0, 2000), x + i, y - i)
 
-      // context.shadowBlur = random.range(50, 200)
-      // context.shadowColor = color6
-      // grd.addColorStop(0, color)
-      // grd.addColorStop(.25, color1)
-      // grd.addColorStop(.5, color3)
-      // grd.addColorStop(.75, color4)
-      // grd.addColorStop(1, color2)
+      let grd = context.createLinearGradient(random.range(0, 100), random.range(0, 2000), x + i, y - i)
       grd.addColorStop(random.range(0, 0.2), color5)
       grd.addColorStop(random.range(0.2, .4), color3)
       grd.addColorStop(random.range(0.4, 0.6), color1)
@@ -249,3 +207,31 @@ const sketch = ({ context, width, height }) => {
 };
 
 canvasSketch(sketch, settings);
+
+class Arc {
+  constructor(startX, startY, radius, startAngle, endAngle) {
+    this.startX = startX,
+      this.startY = startY,
+      this.radius = radius * random.range(1, .4),
+      this.startAngle = startAngle,
+      this.endAngle = endAngle
+  }
+
+  addStops(colorValues) {
+    colorValues.forEach(item => {
+      this.addColorStop(item.stop, item.color)
+    })
+
+  }
+
+  draw(context) {
+    pen.save()
+    // pen.translate(this.pos.x, this.pos.y)
+    pen.beginPath()
+    pen.arc(this.startX, this.startY, this.radius, 0, Math.PI * 2)
+    pen.fill()
+    pen.lineWidth = 4
+    pen.stroke()
+    pen.restore()
+  }
+}
