2765e4d5f309f229d4f6668dd5ed03c66927aac2
Mike Peiman <mikepeiman@gmail.com>
2021-12-01 13:13:08 -0500

    created control button for play/pause, but needs more complex implementation, passing for now

--- src/routes/creative/sketch-02-animate.js
+++ src/routes/creative/sketch-02-animate.js
@@ -4,9 +4,31 @@ const random = require('canvas-sketch-util/random')
 
 const settings = {
   dimensions: [2048, 2048],
-  animate: true
+  animate: false
 };
 
+
+// function createControlButton() {
+
+//   let canvas = document.getElementsByTagName('canvas')
+//   let body = document.body
+//   let insertionPoint = body.children[2]
+//   const button = document.createElement('button')
+//   button.innerText = "Play/Pause"
+//   button.classList = "btn btn-primary btn-outline"
+//   console.log(`🚀 ~ file: sketch-02-animate.js ~ line 18 ~ createControlButton ~ button`, button)
+//   console.log(`🚀 ~ file: sketch-02-animate.js ~ line 13 ~ createControlButton ~ canvas`, canvas)
+//   console.log(`🚀 ~ file: sketch-02-animate.js ~ line 13 ~ createControlButton ~ document`, document)
+//   console.log(`🚀 ~ file: sketch-02-animate.js ~ line 14 ~ createControlButton ~ body`, body)
+//   body.insertBefore(button, insertionPoint)
+//   button.addEventListener('click', () => {
+//     settings['animate'] = true ? settings.animate = true : console.log(`false: `,settings)
+//     console.log(`🚀 ~ file: sketch-02-animate.js ~ line 26 ~ button.addEventListener ~ settings`, settings)
+//     canvasSketch(sketch, settings);
+//   })
+// }
+
+// createControlButton()
 // =============================================================================
 // basic animation example below; canvas-sketch 'animate' setting calls this on main return function
 // =============================================================================
@@ -38,7 +60,6 @@ const sketch = ({ context, width, height }) => {
   let NUM_ARCS = colors.length
   // arcs = []
   for (let i = 0; i < NUM_ARCS; i++) {
-    console.log(`🚀 ~ file: sketch-02-animate.js ~ line 40 ~ sketch ~ i`, i)
     let arc = createArc(cx, cy, random.range(radius * .8, radius * 1.1), random.range(i, 12), i + 1, random.range(55, 250), colors)
     arcs.push(arc)
   }
@@ -51,11 +72,8 @@ const sketch = ({ context, width, height }) => {
     let num = arcs.length
     for (let i = 0; i < arcs.length; i++) {
       radius = width * random.range(0.325, .35)
-      console.log(`🚀 ~ file: sketch-02-animate.js ~ line 54 ~ return ~ radius`, radius)
       slice = math.degToRad(360 / num)
-      console.log(`🚀 ~ file: sketch-02-animate.js ~ line 55 ~ return ~ slice`, slice)
       angle = slice * i
-      console.log(`🚀 ~ file: sketch-02-animate.js ~ line 56 ~ return ~ angle`, angle)
       x = cx + radius * Math.sin(angle)
       y = cy + radius * Math.cos(angle)
       const arc = arcs[i];
@@ -64,9 +82,9 @@ const sketch = ({ context, width, height }) => {
         // pen.lineWidth = math.mapRange(dist, 0, 200, 12, 1)
         pen.beginPath()
         pen.arc(arc.center.x, arc.center.y, arc.radius, arc.angle.start, arc.angle.end)
-        let grd = pen.createLinearGradient(cx,cy,x,y)
-        console.log(`🚀 ~ file: sketch-02-animate.js ~ line 65 ~ return ~ grd`, grd)
-        console.log(`🚀 ~ file: sketch-02-animate.js ~ line 65 ~ return ~ cx,cy,x,y`, cx,cy,x,y)
+        let grd = pen.createLinearGradient(cx, cy, x, y)
+        // console.log(`🚀 ~ file: sketch-02-animate.js ~ line 65 ~ return ~ grd`, grd)
+        // console.log(`🚀 ~ file: sketch-02-animate.js ~ line 65 ~ return ~ cx,cy,x,y`, cx,cy,x,y)
         grd.addColorStop(0, `${arc.colors[i]}`)
         grd.addColorStop(1, transparent)
         pen.strokeStyle = grd
@@ -79,7 +97,7 @@ const sketch = ({ context, width, height }) => {
     }
 
     arcs.forEach(arc => {
-updateArcAngles(arc)
+      updateArcAngles(arc)
     })
   };
 };
