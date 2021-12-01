82c669b79a27e45ebdcfaefc69651359c792f75c
Mike Peiman <mikepeiman@gmail.com>
2021-12-01 13:52:28 -0500

    increasing animation complexity, functioning well

--- src/routes/creative/sketch-02-animate.js
+++ src/routes/creative/sketch-02-animate.js
@@ -4,31 +4,9 @@ const random = require('canvas-sketch-util/random')
 
 const settings = {
   dimensions: [2048, 2048],
-  animate: false
+  animate: true
 };
 
-
-// function createControlButton() {
-
-//   let canvas = document.getElementsByTagName('canvas')
-//   let body = document.body
-//   let insertionPoint = body.children[2]
-//   const button = document.createElement('button')
-//   button.innerText = "Play/Pause"
-//   button.classList = "btn btn-primary btn-outline"
-//   console.log(`🚀 ~ file: sketch-02-animate.js ~ line 18 ~ createControlButton ~ button`, button)
-//   console.log(`🚀 ~ file: sketch-02-animate.js ~ line 13 ~ createControlButton ~ canvas`, canvas)
-//   console.log(`🚀 ~ file: sketch-02-animate.js ~ line 13 ~ createControlButton ~ document`, document)
-//   console.log(`🚀 ~ file: sketch-02-animate.js ~ line 14 ~ createControlButton ~ body`, body)
-//   body.insertBefore(button, insertionPoint)
-//   button.addEventListener('click', () => {
-//     settings['animate'] = true ? settings.animate = true : console.log(`false: `,settings)
-//     console.log(`🚀 ~ file: sketch-02-animate.js ~ line 26 ~ button.addEventListener ~ settings`, settings)
-//     canvasSketch(sketch, settings);
-//   })
-// }
-
-// createControlButton()
 // =============================================================================
 // basic animation example below; canvas-sketch 'animate' setting calls this on main return function
 // =============================================================================
@@ -57,7 +35,7 @@ const sketch = ({ context, width, height }) => {
   let x, y
 
   let colors = generateVariedColors()
-  let NUM_ARCS = colors.length
+  let NUM_ARCS = 45
   // arcs = []
   for (let i = 0; i < NUM_ARCS; i++) {
     let arc = createArc(cx, cy, random.range(radius * .8, radius * 1.1), random.range(i, 12), i + 1, random.range(55, 250), colors)
@@ -85,8 +63,10 @@ const sketch = ({ context, width, height }) => {
         let grd = pen.createLinearGradient(cx, cy, x, y)
         // console.log(`🚀 ~ file: sketch-02-animate.js ~ line 65 ~ return ~ grd`, grd)
         // console.log(`🚀 ~ file: sketch-02-animate.js ~ line 65 ~ return ~ cx,cy,x,y`, cx,cy,x,y)
-        grd.addColorStop(0, `${arc.colors[i]}`)
-        grd.addColorStop(1, transparent)
+        grd.addColorStop(0, `${arc.colors[wrapIndex(colors, i)]}`)
+        grd.addColorStop(.3, `${arc.colors[wrapIndex(colors, i+2)]}`)
+        grd.addColorStop(.6, `${arc.colors [wrapIndex(colors, i+4)]}`)
+        grd.addColorStop(.9, `${arc.colors [wrapIndex(colors, i+7)]}`)
         pen.strokeStyle = grd
         pen.lineWidth = arc.lineWidth
         // pen.moveTo(arc.startPos.x, arc.startPos.y)
@@ -97,75 +77,14 @@ const sketch = ({ context, width, height }) => {
     }
 
     arcs.forEach(arc => {
-      updateArcAngles(arc)
+      updateArc(arc)
+      bounce(arc, width)
+      // console.log(`🚀 ~ file: sketch-02-animate.js ~ line 103 ~ return ~ arc`, arc)
     })
   };
 };
 
 canvasSketch(sketch, settings);
-
-
-class Origin {
-  constructor(x, y) {
-    this.x = x
-    this.y = y
-  }
-
-  getDistance(v) {
-    const dx = this.x - v.x
-    const dy = this.y - v.y
-    return Math.sqrt(dx * dx + dy * dy)
-  }
-
-  addColorStop(color, stop) {
-    this.color = color
-    this.stop = stop
-    this.addColorStop(stop, color)
-  }
-}
-
-class Arc {
-  constructor(x1, y1, x2, y2) {
-    this.startPos = new Origin(x1, y1)
-    this.endPos = new Origin(x2, y2)
-    this.vel = new Origin(random.range(-1, 1), random.range(-1, 1))
-    this.radius = random.range(4, 12)
-  }
-  update() {
-    this.startPos.x += this.vel.x
-    this.startPos.y += this.vel.y
-  }
-
-  bounce(width, height) {
-    if (this.startPos.x <= 0 || this.startPos.x >= width) {
-      this.vel.x *= -1
-    }
-    if (this.startPos.y <= 0 || this.startPos.y >= height) {
-      this.vel.y *= -1
-    }
-  }
-
-  wrap(width, height) {
-    if (this.startPos.x > width) this.startPos.x = 0
-    if (this.startPos.x < 0) this.startPos.x = width
-    if (this.startPos.y > height) this.startPos.y = 0
-    if (this.startPos.y < 0) this.startPos.y = height
-  }
-  setGrad(pen) {
-
-  }
-
-  draw(pen) {
-    pen.save()
-    pen.translate(this.startPos.x, this.startPos.y)
-    pen.beginPath()
-    pen.arc(0, 0, this.radius, 0, Math.PI * 2)
-    pen.fill()
-    pen.lineWidth = 4
-    pen.stroke()
-    pen.restore()
-  }
-}
 
 function generateVariedColors(s1 = 25, s2 = 75, l1 = 25, l2 = 75, a1 = 0, a2 = 1) {
   let variedHues = []
@@ -187,7 +106,12 @@ function createArc(x, y, r, s, e, lineWidth, colors) {
     radius: r,
     angle: { start: s, end: e },
     lineWidth: lineWidth,
-    colors: colors
+    colors: colors,
+    vel: {
+      radius: (random.range(5, 2.5)),
+      rotation: (random.range(0.25, .001)),
+      hueChange: (random.range(0.001, 5))
+    }
   }
   return arc
 }
@@ -196,8 +120,108 @@ function updateArcColor() {
 
 }
 
-function updateArcAngles(arc) {
-  arc.angle.start += .01
-  arc.angle.end += .01
+function bounce(arc, width) {
+  let r = arc.radius
+  let minRadius = width * .1
+  let maxRadius = width * .5
+  if (r <= minRadius || r >= maxRadius) {
+    arc.vel.radius *= -1
+    arc.vel.rotation = (random.range(0.25, .001))
+  }
 }
 
+function updateArc(arc) {
+  arc.angle.start += parseFloat(arc.vel.rotation)
+  arc.angle.end += parseFloat(arc.vel.rotation)
+  arc.radius += parseFloat(arc.vel.radius)
+}
+
+const wrapIndex = (arr, index) => {
+  return index % arr.length
+}
+
+// class Origin {
+//   constructor(x, y) {
+//     this.x = x
+//     this.y = y
+//   }
+
+//   getDistance(v) {
+//     const dx = this.x - v.x
+//     const dy = this.y - v.y
+//     return Math.sqrt(dx * dx + dy * dy)
+//   }
+
+//   addColorStop(color, stop) {
+//     this.color = color
+//     this.stop = stop
+//     this.addColorStop(stop, color)
+//   }
+// }
+
+// class Arc {
+//   constructor(x1, y1, x2, y2) {
+//     this.startPos = new Origin(x1, y1)
+//     this.endPos = new Origin(x2, y2)
+//     this.vel = new Origin(random.range(-1, 1), random.range(-1, 1))
+//     this.radius = random.range(4, 12)
+//   }
+//   update() {
+//     this.startPos.x += this.vel.x
+//     this.startPos.y += this.vel.y
+//   }
+
+//   bounce(width, height) {
+//     if (this.startPos.x <= 0 || this.startPos.x >= width) {
+//       this.vel.x *= -1
+//     }
+//     if (this.startPos.y <= 0 || this.startPos.y >= height) {
+//       this.vel.y *= -1
+//     }
+//   }
+
+//   wrap(width, height) {
+//     if (this.startPos.x > width) this.startPos.x = 0
+//     if (this.startPos.x < 0) this.startPos.x = width
+//     if (this.startPos.y > height) this.startPos.y = 0
+//     if (this.startPos.y < 0) this.startPos.y = height
+//   }
+//   setGrad(pen) {
+
+//   }
+
+//   draw(pen) {
+//     pen.save()
+//     pen.translate(this.startPos.x, this.startPos.y)
+//     pen.beginPath()
+//     pen.arc(0, 0, this.radius, 0, Math.PI * 2)
+//     pen.fill()
+//     pen.lineWidth = 4
+//     pen.stroke()
+//     pen.restore()
+//   }
+// }
+
+
+
+// function createControlButton() {
+
+//   let canvas = document.getElementsByTagName('canvas')
+//   let body = document.body
+//   let insertionPoint = body.children[2]
+//   const button = document.createElement('button')
+//   button.innerText = "Play/Pause"
+//   button.classList = "btn btn-primary btn-outline"
+//   console.log(`🚀 ~ file: sketch-02-animate.js ~ line 18 ~ createControlButton ~ button`, button)
+//   console.log(`🚀 ~ file: sketch-02-animate.js ~ line 13 ~ createControlButton ~ canvas`, canvas)
+//   console.log(`🚀 ~ file: sketch-02-animate.js ~ line 13 ~ createControlButton ~ document`, document)
+//   console.log(`🚀 ~ file: sketch-02-animate.js ~ line 14 ~ createControlButton ~ body`, body)
+//   body.insertBefore(button, insertionPoint)
+//   button.addEventListener('click', () => {
+//     settings['animate'] = true ? settings.animate = true : console.log(`false: `,settings)
+//     console.log(`🚀 ~ file: sketch-02-animate.js ~ line 26 ~ button.addEventListener ~ settings`, settings)
+//     canvasSketch(sketch, settings);
+//   })
+// }
+
+// createControlButton()
