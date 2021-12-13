d7a3296f4ac37640d2c9bf4773637bf99d3ef726
Mike Peiman <mikepeiman@gmail.com>
2021-12-13 10:37:43 -0500

    failing to get reactive resizing properly; buggy; must be missing something obvious

--- src/routes/sketch02.svelte
+++ src/routes/sketch02.svelte
@@ -4,6 +4,9 @@
 	import Color from '$components/Color.svelte';
 	import Checkbox from '$components/Checkbox.svelte';
 	import { drawRect, setItemColor } from '../lib/drawing';
+	import { onMount } from 'svelte';
+	import { browser } from '$app/env';
+
 	const data = {
 		itemHeight: 25,
 		itemWidth: 25,
@@ -14,8 +17,8 @@
 		originX: 0,
 		originY: 0,
 		totalItems: false,
-		remainingWidth: 0,
-		remainingHeight: 0,
+		remainingWidth: 1080,
+		remainingHeight: 1080,
 		offset: 0,
 		stroke: 20,
 		outline: true,
@@ -28,9 +31,29 @@
 		lineWidth: 20
 	};
 
-	let width, height, fill, stroke;
+	let width = 1080;
+	let height = 1080;
+	let canvas = {}
+	canvas['width'] = width
+	canvas['height'] = height
+	let fill, stroke;
 
 	$: data.totalItems = data.itemsPerColumn * data.itemsPerRow;
+	$: {
+		// data.remainingWidth = width - data.itemsPerRow * (data.gap + data.itemWidth);
+		data.itemWidth = data.remainingWidth / data.itemsPerRow;
+		console.log(`🚀 ~ file: sketch02.svelte ~ line 36 ~ data.remainingWidth`, data.remainingWidth);
+		console.log(`🚀 ~ file: sketch02.svelte ~ line 36 ~ data.itemWidth`, data.itemWidth);
+		console.log(`🚀 ~ file: sketch02.svelte ~ line 36 ~ data.gap`, data.gap);
+		console.log(`🚀 ~ file: sketch02.svelte ~ line 36 ~ data.itemsPerRow`, data.itemsPerRow);
+		console.log(`🚀 ~ file: sketch02.svelte ~ line 36 ~ width`, width);
+	}
+	$: if (browser) {
+		console.log(`browser`);
+		width = canvas.width;
+		data.remainingWidth = width - data.itemsPerRow * (data.gap + data.itemWidth);
+	}
+	// $: console.log(`🚀 ~ file: sketch02.svelte ~ line 35 ~ data.remainingWidth`, data.remainingWidth)
 	// $: data.marginsX = width * data.marginValue
 	// $: data.marginsY = width * data.marginValue
 
@@ -39,10 +62,18 @@
 		dimensions: [1280, 1280]
 	};
 
+	onMount(() => {
+		let canvas = document.getElementsByTagName('canvas')[0];
+		width = canvas.width;
+		height = canvas.height;
+		console.log(`🚀 ~ file: sketch02.svelte ~ line 50 ~ canvas`, canvas);
+	});
+
 	const sketch = ({ context, width, height }) => {
 		data.remainingWidth = width - data.itemsPerRow * data.gap;
 		data.remainingHeight = height - data.itemsPerColumn * data.gap;
-
+		width = width;
+		height = height;
 		data.itemWidth = data.remainingWidth / data.itemsPerRow;
 		data.itemHeight = data.remainingHeight / data.itemsPerColumn;
 
@@ -50,7 +81,10 @@
 		data.remainingHeight = height - data.itemsPerColumn * (data.gap + data.itemHeight);
 
 		data.offset = data.gap / 2;
-        console.log(`🚀 ~ file: sketch02.svelte ~ line 49 ~ sketch ~ data.remainingWidth`, data.remainingWidth)
+		console.log(
+			`🚀 ~ file: sketch02.svelte ~ line 49 ~ sketch ~ data.remainingWidth`,
+			data.remainingWidth
+		);
 		// data.originX = data.offset
 		// data.originY = data.offse
 		console.log(`🚀 ~ file: sketch02.svelte ~ line 46 ~ sketch ~ data.offset `, data.offset);
@@ -65,19 +99,13 @@
 	};
 
 	function drawGrid(context, width, height) {
-		console.log(data);
+		// console.log(data);
 		for (let j = 0; j < data.itemsPerColumn; j++) {
 			for (let i = 0; i < data.itemsPerRow; i++) {
 				fill = `hsla(180, 50%, 50%, .4)`;
 				let x = data.originX + (data.itemWidth + data.gap) * i;
 				let y = data.originY + (data.itemHeight + data.gap) * j;
 				stroke = 'white';
-
-				console.log(
-					`🚀 ~ file: sketch02.svelte ~ line 69 ~ drawGrid ~ data.itemWidth - data.offset`,
-					data.itemWidth,
-					data.offset
-				);
 				drawRect(
 					context,
 					x + data.offset,
